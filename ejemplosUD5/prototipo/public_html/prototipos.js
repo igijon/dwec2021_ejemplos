7f978134f373bbc36c4ef3e5c917bcb04ae20d72
inmagijon <igijoninf@gmail.com>
2020-10-25 18:52:45 +0100

    Added example of prototypes

---
+++ ejemplosUD5/prototipo/public_html/prototipos.js
@@ -0,0 +1,24 @@
+/* 
+ * Un constructor en JavaScript es una función que crea un objeto. Por convención, 
+ * el nombre de esta función siempre empieza con mayúsculas y la creación del objeto 
+ * se realiza con el operador new.
+ * 
+ * Cuando se utiliza una función como constructor , el prototipo del objeto 
+ * creado referencia al de la función constructora, por lo que este hereda todas 
+ * las propiedades y métodos establecidos en el prototipo. Por consiguiente, sólo 
+ * los objetos generados a partir de este patrón tienen acceso al prototipo 
+ * automáticamente.
+ */
+
+var Persona = function (nombre) {
+    this.nombre = nombre;
+};
+
+Persona.prototype.saludar = function () {
+    console.log('Hola, mi nombre es ' + this.nombre);
+};
+
+var jugador = new Persona('Ricardo');
+jugador.saludar();
+
+
